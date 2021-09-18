d6be2334177956c426a257e58ca6a979829d76c8
iwamottyan <jhrauihfrau@gmail.com>
2021-09-19 06:52:28 +0900

    フロント実装とログイン画面の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configuer_permitted_parameters, if: :devise_controller?
+
+  private
+  def configuer_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name])
+  end
 end
---
+++ app/controllers/furima_controller.rb
@@ -0,0 +1,6 @@
+class FurimaController < ApplicationController
+  #before_action :authenticate_user!
+  def index
+    
+  end
+end
