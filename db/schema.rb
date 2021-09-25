946d1cbd12e7adecf537dcbdf7f74f70b0887074
iwamottyan <jhrauihfrau@gmail.com>
2021-09-25 20:53:56 +0900

    ユーザー認証機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2021_09_08_144636) do
     t.string "last_name", null: false
     t.string "first_name_kana", null: false
     t.string "last_name_kana", null: false
-    t.string "birthday", null: false
+    t.date "birthday", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
