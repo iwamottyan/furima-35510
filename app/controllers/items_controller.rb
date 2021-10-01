491f845c73444ff7087c9a6a29f7954d21d6e0ea
iwamottyan <jhrauihfrau@gmail.com>
2021-10-01 20:41:45 +0900

    画像の投稿とActiveHashの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
 class ItemsController < ApplicationController
   #before_action :authenticate_user!
-  #def index
-    #@items = Item.includes(:user)
-  #end
+  def index
+    @items = Item.includes(:user)
+  end
 
   def new
     @item = Item.new
@@ -12,5 +12,5 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
-
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,17 @@ class Item < ApplicationRecord
   belongs_to :user
   #has_one :buyer
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :status, :postate, :area, :day
+
+  validates :image, presence: true
+  validates :item_name, presence: true
+  validates :item_text, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "category can't be blank"}
+  validates :status_id, numericality: { other_than: 1 , message: "status can't be blank"}
+  validates :postage, numericality: { other_than: 1 , message: "postage  can't be blank"}
+  validates :area, numericality: { other_than: 1 , message: "area can't be blank"}
+  validates :day, numericality: { other_than: 1 , message: "day can't be blank"}
+  validates :price, presence: true
 end
