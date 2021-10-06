c055aaa558dcfaa8b40357ff078ca28efe5caae1
iwamottyan <jhrauihfrau@gmail.com>
2021-10-06 19:45:39 +0900

    詳細画面への遷移

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # devise_for :items
   devise_for :users
   root to: "items#index"
-  resources :items, only: [:new, :create] do
+  resources :items, only: [:new, :create, :show] do
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
