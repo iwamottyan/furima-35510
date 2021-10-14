a7feb37b5919aeed0db1b2139f354bd5da2c03ca
iwamottyan <jhrauihfrau@gmail.com>
2021-10-14 20:40:15 +0900

    ordersコントローラーの作成

---
+++ app/models/cardinfo_address.rb
@@ -0,0 +1,4 @@
+class CardinfoAddress
+  include ActiveModel::Model
+  attr_accessor :image, :item_name, :item_text, :category_id, :status_id, :postage_id, :area_id, :item_day_id, :price, :user_id
+end
